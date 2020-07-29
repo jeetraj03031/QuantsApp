a0d67c72d089523fd69a48916358fe8d2bdc8e3b
JITENDRA <jrajput@epicchannel.com>
2020-07-29 22:45:20 +0530

    Initial Commit

---
+++ QuantsAppTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuantsAppTest
+//
+//  Created by EPIC CHANNEL on 29/07/20.
+//  Copyright © 2020 QuantsApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
